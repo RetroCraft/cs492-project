45316fcd0b2f16824bf01de6890bb80ae3413ad9
James Ah Yong <james@retrocraft.ca>
2023-04-10 01:12:18 -0400

    set up first visualization

--- src/components/FemaleCounter.js
+++ src/components/FemaleCounter.js
@@ -1,14 +1,46 @@
 import React, { useState } from 'react';
 import { Scrollama, Step } from 'react-scrollama';
+import { VictoryBar, VictoryChart, VictoryScatter } from 'victory';
 
 const ScrollamaDemo = () => {
-  const [currentStepIndex, setCurrentStepIndex] = useState(null);
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
 
   // This callback fires when a Step hits the offset threshold. It receives the
   // data prop of the step, which in this demo stores the index of the step.
   const onStepEnter = ({ data }) => {
     setCurrentStepIndex(data);
   };
+
+  const generateData = () => {
+    const data = []
+    if (currentStepIndex === 0) {
+      // draw 100 students in a grid
+      for (let i = 0; i < 100; i++) {
+        data.push({
+          x: Math.floor(i / 10) * 10,
+          y: i % 10 * 10
+        })
+      }
+    } else {
+      const numberOfWomen = [50, 30, 20][currentStepIndex - 1];
+      // draw grid of women on left-hand side and grid of men on right-hand side
+      for (let i = 0; i < numberOfWomen; i++) {
+        data.push({
+          x: Math.floor(i / 10) * 10,
+          y: i % 10 * 10,
+          fill: 'pink'
+        })
+      }
+      for (let i = 0; i < 100 - numberOfWomen; i++) {
+        data.push({
+          x: 100 + Math.floor(i / 10) * 10,
+          y: i % 10 * 10,
+          fill: 'blue'
+        })
+      }
+    }
+    return data;
+  }
 
   return (
     <div>
@@ -17,36 +49,69 @@ const ScrollamaDemo = () => {
         display: 'flex', alignItems: 'center', justifyContent: 'center',
         zIndex: -100
       }}>
-        <div>
-          imagine that we've rendered {currentStepIndex} women back here
+        <div className="container-fluid">
+          <VictoryScatter
+            data={generateData()}
+            animate={{ duration: 500 }}
+            size={10}
+            style={{
+              data: {
+                fill: ({ datum }) => datum.fill
+              }
+            }}
+          />
         </div>
       </div>
       <Scrollama offset={0.5} onStepEnter={onStepEnter}>
-        {[1, 2, 3].map((stepIndex) => (
-          <Step data={stepIndex} key={stepIndex}>
-            <div
-              style={{
-                padding: '25vh 0',
-                opacity: currentStepIndex === stepIndex ? 1 : 0.2,
-                zIndex: '100'
-              }}
-            >
-              <div className="row p-4 align-items-center rounded-3 border shadow-lg bg-white">
-                I'm a Scrollama Step of index {stepIndex}
-              </div>
-            </div>
-          </Step>
-        ))}
-        <Step data={4}>
+        <Step data={0}>
           <div
             style={{
               padding: '25vh 0 50vh 0',
-              opacity: currentStepIndex === 4 ? 1 : 0.2,
+              opacity: currentStepIndex === 0 ? 1 : 0.2,
               zIndex: '100'
             }}
           >
             <div className="row p-4 align-items-center rounded-3 border shadow-lg bg-white">
-              I'm a Scrollama Step of index 4
+              Consider a group of 100 post-secondary students.
+            </div>
+          </div>
+        </Step>
+        <Step data={1}>
+          <div
+            style={{
+              padding: '25vh 0 50vh 0',
+              opacity: currentStepIndex === 1 ? 1 : 0.2,
+              zIndex: '100'
+            }}
+          >
+            <div className="row p-4 align-items-center rounded-3 border shadow-lg bg-white">
+              In an average post-secondary class, women make up over half of the students.
+            </div>
+          </div>
+        </Step>
+        <Step data={2}>
+          <div
+            style={{
+              padding: '25vh 0 50vh 0',
+              opacity: currentStepIndex === 2 ? 1 : 0.2,
+              zIndex: '100'
+            }}
+          >
+            <div className="row p-4 align-items-center rounded-3 border shadow-lg bg-white">
+              However, if we consider only STEM classes, this drops to 30%.
+            </div>
+          </div>
+        </Step>
+        <Step data={3}>
+          <div
+            style={{
+              padding: '25vh 0 50vh 0',
+              opacity: currentStepIndex === 3 ? 1 : 0.2,
+              zIndex: '100'
+            }}
+          >
+            <div className="row p-4 align-items-center rounded-3 border shadow-lg bg-white">
+              Even worse, if this is a computer science class, we can expect as few as 20 women.
             </div>
           </div>
         </Step>
